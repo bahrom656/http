5c05bdc18673a97e541e23c90e57452fc6cc90f1
bahrullo <bahrom656>
2020-11-09 11:37:44 +0500

    22.1

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,76 +1,69 @@
 package server
 
 import (
-	"strings"
 	"bytes"
 	"io"
 	"log"
 	"net"
+	"net/url"
+	"strings"
 	"sync"
 )
 
-type Handlerfunc func(conn net.Conn)
+type HandlerFunc func(conn net.Conn)
+
+type RequestFunc func(req *Request)
+
+type Request struct {
+	Conn        net.Conn
+	QueryParams url.Values
+}
 
 type Server struct {
 	addr     string
 	mu       sync.RWMutex
-	handlers map[string]Handlerfunc
+	handlers map[string]RequestFunc
 }
 
 func NewServer(addr string) *Server {
-	return &Server{addr: addr, handlers: make(map[string]Handlerfunc)}
+	return &Server{addr: addr, handlers: make(map[string]RequestFunc)}
 }
-func (s *Server) Register(path string, handler Handlerfunc) {
+func (s *Server) Register(path string, request RequestFunc) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	s.handlers[path] = handler
+
+	s.handlers[path] = request
 }
 func (s *Server) Start() (err error) {
+	listener, err := net.Listen("tcp", s.addr)
 
-		
-		listener, err := net.Listen("tcp", s.addr)
-		
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if cerr := listener.Close(); cerr != nil {
+			if err == nil {
+				err = cerr
+				return
+			}
+			log.Print(cerr)
+		}
+	}()
+	for {
+		conn, err := listener.Accept()
 		if err != nil {
-			return err
+			log.Print(err)
+			continue
 		}
-	
-		defer func() {
-			if cerr := listener.Close(); cerr != nil {
-				if err == nil {
-					err = cerr
-					return
-				}
-				log.Print(cerr)
-			}
-		}()	
-		for {
-			conn, err := listener.Accept()
-			if err != nil {
-				log.Print(err)
-				continue
-			}
-			err = s.handle(conn)
-			if err != nil{
-				continue
-			}
+		err = s.handle(conn)
+		if err != nil {
+			continue
 		}
+	}
 }
 
-
-		// for {
-		// 	conn, err := listener.Accept()
-		// 	if err != nil {
-		// 		log.Print(err)
-		// 		continue
-		// 	}
-		// 	go handle(conn)
-			
-		// }
-	
-
-
-
-func(s *Server) handle(conn net.Conn) (err error) {
+func (s *Server) handle(conn net.Conn) (err error) {
 	defer func() {
 		if cerr := conn.Close(); cerr != nil {
 			if err == nil {
@@ -92,36 +85,42 @@ func(s *Server) handle(conn net.Conn) (err error) {
 		return err
 	}
 	data := buf[:n]
-	requestlinedelim := []byte{'\r', '\n'}
-	requestlineend := bytes.Index(data, requestlinedelim)
-	if requestlineend == -1 {
+	requestLineDelimiter := []byte{'\r', '\n'}
+	requestLineEnd := bytes.Index(data, requestLineDelimiter)
+	if requestLineEnd == -1 {
 		log.Print("error line end")
 		return
 	}
 
-	requestline := string(data[:requestlineend])
-	parts := strings.Split(requestline, " ")
+	requestLine := string(data[:requestLineEnd])
+	parts := strings.Split(requestLine, " ")
 
 	if len(parts) != 3 {
 		log.Print("error parts")
 		return
 	}
 
-	_, path, version := parts[0], parts[1], parts[2]
-	// if method != "GET"{
-	// 	log.Print("error get")
-	// 	return
-	// }
+	method, pathWithQuery, version := parts[0], parts[1], parts[2]
+	if method != "GET" {
+		log.Print("error get")
+	}
 
-	if version != "HTTP/1.1"{
+	uri, err := url.ParseRequestURI(pathWithQuery)
+	if err != nil {
+		log.Println("error parse request uri:", err)
+	}
+
+	if version != "HTTP/1.1" {
 		log.Print(err)
 		return
 	}
-	for pat, handler := range s.handlers{
-		if path == pat{
-			handler(conn)
+	for pat, handler := range s.handlers {
+		if uri.Path == pat {
+			handler(&Request{
+				Conn:        conn,
+				QueryParams: uri.Query(),
+			})
 		}
 	}
 	return nil
 }
-
